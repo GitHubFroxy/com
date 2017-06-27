56127c558a35a6b9206c4817339a139e31bab52a
yangchongxin <yangchongxin@126.com>
2017-06-27 22:12:57 +0800

    <优化>优化启动界面功能

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "frmcomtool.h"
 #include <QApplication>
 #include "myhelper.h"
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
@@ -8,19 +9,21 @@ int main(int argc, char *argv[])
     a.setWindowIcon(QIcon(":/main.ico"));
     myHelper::setUTF8Code();
 
-    QSplashScreen *splash=new QSplashScreen;
-    QPixmap pix(":/main.ico");
-
-    splash->setPixmap(pix);
-    splash->show();
-    a.processEvents();//这样可以保证程序一直可以响应鼠标键盘上事件
-    splash->showMessage("正在载入，请稍等……");
-
-    QDateTime n=QDateTime::currentDateTime();
-    QDateTime now;
-    do{
-        now=QDateTime::currentDateTime();
-    } while (n.secsTo(now)<=2);//3为需要延时的秒数
+    QPixmap pixmap(":/start.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+    splash.showMessage("正在载入，请稍等……",Qt::AlignRight|Qt::AlignBottom,Qt::darkBlue);
+    QThread::msleep(1000);
+//    a.processEvents();//这样可以保证程序一直可以响应鼠标键盘上事件
+    splash.showMessage("建立连接，请稍等……",Qt::AlignRight|Qt::AlignBottom,Qt::darkBlue);
+    QThread::msleep(500);
+//    QDateTime n=QDateTime::currentDateTime();
+//    QDateTime now;
+//    do{
+//        now=QDateTime::currentDateTime();
+//        a.processEvents();//这样可以保证程序一直可以响应鼠标键盘上事件
+//    } while (n.secsTo(now)<=10);//3为需要延时的秒数
+//    a.processEvents();//这样可以保证程序一直可以响应鼠标键盘上事件
 
     frmComTool w;
     w.show();
